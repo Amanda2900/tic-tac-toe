a49870dd64024b1c8f9506fe676505309c522af7
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-10-07 19:17:41 -0400

    add variables and cached elements

--- js/app.js
+++ js/app.js
@@ -3,11 +3,20 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let boardSquares, turnOrder, isWinnner;
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const topLeft = document.querySelector(".top-left");
+const topCenter = document.querySelector(".top-center");
+const topRight = document.querySelector(".top-right");
+const centerLeft = document.querySelector(".center-left");
+const centerCenter = document.querySelector(".center-center");
+const centerRight = document.querySelector(".center-right");
+const bottomleft = document.querySelector(".bottom-left");
+const bottomCenter = document.querySelector(".bottom-center");
+const bottomRight = document.querySelector(".bottom-right");
+const gameStatus = document.querySelector("#message")
 
 
 /*----------------------------- Event Listeners -----------------------------*/
