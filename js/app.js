72713debcdc5d8510a735eab0c0a1f6722ee4428
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-10-08 16:48:51 -0400

    forgot to push - add render, handleClick, getWinner

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [0, 1, 2], //top across
+  [3, 4, 5], //center across
+  [6, 7, 8], //bottom across
+  [0, 3, 6], //left down
+  [1, 4, 7], //center down
+  [3, 5, 8], //right down
+  [0, 4, 8], //diagonal l-r
+  [2, 4, 6] //diagonal r-l
+];
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -7,45 +16,88 @@ let boardSquares, turnOrder, winner;
 
 
 /*------------------------ Cached Element References ------------------------*/
-const topLeft = document.querySelector("#sq0");
-const topCenter = document.querySelector("#sq1");
-const topRight = document.querySelector("#sq2");
-const centerLeft = document.querySelector("#sq3");
-const centerCenter = document.querySelector("#sq4");
-const centerRight = document.querySelector("#sq5");
-const bottomleft = document.querySelector("#sq6");
-const bottomCenter = document.querySelector("#sq7");
-const bottomRight = document.querySelector("#sq8");
-const gameStatus = document.querySelector("#message");
 
-const squares = [...document.querySelectorAll('.square')].map((square) => square.innerText); //converts the static node list to an array of items
+const gameStatus = document.querySelector('#message');
 
+const squares = document.querySelectorAll('.square'); 
+const board = document.querySelector('.board');
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+board.addEventListener('click', handleClick);
 
 
 /*-------------------------------- Functions --------------------------------*/
+
 init();
 
 function init() {
   boardSquares = [null, null, null, null, null, null, null, null];
   turnOrder = 1; //1 is player X, -1 is player O
   winner = null; // will hold either 1, -1, or T for tie
+
+  render();
 };
 
-render();
-
 function render() {
-  for (i = 0; i < boardSquares.length; i++) {
-    if (squares[i] === 'X'){
+  for (i = 0; i <= boardSquares.length; i++) {
+    if (squares[i].innerText === 'X'){
       boardSquares[i] = 1;
-    } else if(squares[i] === 'O') {
+      squares[i].style.backgroundColor = "blue";
+    } else if(squares[i].innerText === 'O') {
       boardSquares[i] = -1;
+      squares[i].style.backgroundColor = "red";
     } else {
       boardSquares[i] = null;
     }
+    if (winner === 1) {
+      'X has won the game!';
+    } else if (winner === -1) {
+      'O has won the game!';
+    } else if (winner === 'T') {
+      'The games ends in a tie!';
+    } else {
+      'The game is still in progress. It is (players) turn';
+    }
   };
-  console.log(boardSquares)
 };
-console.log(squares);
+
+function handleClick(event) {
+
+  let i = parseInt(event.target.id);
+
+  if (boardSquares[i] !== null) {
+    return;
+  }
+  if (winner !== null) {
+    return;
+  }
+  if (boardSquares[i] === null && turnOrder === 1) {
+    event.target.innerText = 'X';
+    gameStatus.innerText = "It is O's turn"
+  }
+  if (boardSquares[i] === null && turnOrder === -1) {
+    event.target.innerText = 'O';
+    gameStatus.innerText = "It is X's turn"
+  }
+
+  boardSquares[i] = turnOrder; // update board index with the value of turn
+  turnOrder = turnOrder * -1;
+  render();
+}
+
+function getWinner() {
+  winningCombos.forEach(function (array) {
+    let counter = 0;
+    array.forEach(function (element) {
+      counter += boardSquares[element];
+      if (Math.abs(counter) === 3) {
+        winner === boardSquares[element]
+        console.log(counter)
+      }
+      else {
+        
+        return null;
+      }
+    })
+  }) 
+}
