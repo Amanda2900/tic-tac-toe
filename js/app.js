30a25a5eba6c9f70cf606a1496e095cc8c323b6e
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-10-07 21:13:08 -0400

    add part of render function

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-let boardSquares, turnOrder, isWinnner;
+let boardSquares, squares, turnOrder, winner;
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -16,7 +16,7 @@ const centerRight = document.querySelector(".center-right");
 const bottomleft = document.querySelector(".bottom-left");
 const bottomCenter = document.querySelector(".bottom-center");
 const bottomRight = document.querySelector(".bottom-right");
-const gameStatus = document.querySelector("#message")
+const gameStatus = document.querySelector("#message");
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -24,4 +24,28 @@ const gameStatus = document.querySelector("#message")
 
 
 /*-------------------------------- Functions --------------------------------*/
+init();
 
+squares = [topLeft.innerText, topCenter.innerText, topRight.innerText, centerLeft.innerText, centerCenter.innerText, centerRight.innerText, bottomleft.innerText, bottomCenter.innerText, bottomRight.innerText];
+
+function init() {
+  boardSquares = [null, null, null, null, null, null, null, null, null];
+  turnOrder = 1; //1 is player X, -1 is player O
+  winner = null; // will hold either 1, -1, or T for tie
+};
+
+render();
+
+function render() {
+  for (i = 0; i < boardSquares.length; i++) {
+    if (squares[i] === 'X'){
+      boardSquares[i] = 1;
+    } else if(squares[i] === 'O') {
+      boardSquares[i] = -1;
+    } else {
+      boardSquares[i] = null;
+    }
+  };
+  console.log(boardSquares)
+};
+console.log(squares);
