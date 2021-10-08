30d364fb4efeab3e4a65345f7ad2657043f9c95f
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-10-08 19:22:09 -0400

    add reset button

--- js/app.js
+++ js/app.js
@@ -18,13 +18,14 @@ let boardSquares, turnOrder, winner;
 /*------------------------ Cached Element References ------------------------*/
 
 const gameStatus = document.querySelector('#message');
-
+const replayBtn = document.querySelector('#replay-button');
 const squares = document.querySelectorAll('.square'); 
 const board = document.querySelector('.board');
+const form = document.querySelector("form");
 
 /*----------------------------- Event Listeners -----------------------------*/
 board.addEventListener('click', handleClick);
-
+form.addEventListener("reset", init);
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -34,29 +35,32 @@ function init() {
   boardSquares = [null, null, null, null, null, null, null, null, null];
   turnOrder = 1; //1 is player X, -1 is player O
   winner = null; // will hold either 1, -1, or T for tie
-
+  gameStatus.innerText = "Let's play! X goes first";
+  squares.forEach(element => element.innerText = '');
+  squares.forEach(element => element.style.backgroundColor = '');
   render();
 };
 
 function render() {
+
   for (i = 0; i < boardSquares.length; i++) {
     if (squares[i].innerText === 'X'){
       boardSquares[i] = 1;
-      squares[i].style.backgroundColor = "blue";
+      squares[i].style.backgroundColor = 'blue';
     } else if(squares[i].innerText === 'O') {
       boardSquares[i] = -1;
-      squares[i].style.backgroundColor = "red";
+      squares[i].style.backgroundColor = 'red';
     } else {
       boardSquares[i] = null;
     }
     if (winner === 1) {
-      'X has won the game!';
-    } else if (winner === -1) {
-      'O has won the game!';
-    } else if (winner === 'T') {
-      'The games ends in a tie!';
-    } else {
-      'The game is still in progress. It is (players) turn';
+      gameStatus.innerText = 'X has won the game!';
+    } 
+    if (winner === -1) {
+      gameStatus.innerText = 'O has won the game!';
+    } 
+    if (winner === 'T') {
+      gameStatus.innerText = 'The games ends in a tie!';
     }
   };
 };
@@ -68,7 +72,7 @@ function handleClick(event) {
   if (boardSquares[i] !== null) {
     return;
   }
-  if (winner !== null) {
+  if (winner !== null || winner === 'T') {
     return;
   }
   if (boardSquares[i] === null && turnOrder === 1) {
@@ -82,6 +86,7 @@ function handleClick(event) {
 
   boardSquares[i] = turnOrder; // update board index with the value of turn
   turnOrder = turnOrder * -1;
+  getWinner();
   render();
 }
 
@@ -92,7 +97,7 @@ function getWinner() {
       counter += boardSquares[element];
       if (Math.abs(counter) === 3) {
         winner = boardSquares[element];
-      }else if(boardSquares.includes(null) === false && winner !== null) {
+      } else if (boardSquares.includes(null) === false && winner === null) {
         winner = 'T';
       }else {
         return null;
