603584d36dab8d1b28019bf11c04eb235fae47f3
Amanda DiNoto <amanda.dinoto@gmail.com>
2021-10-08 20:30:25 -0400

    add comments

--- js/app.js
+++ js/app.js
@@ -32,10 +32,16 @@ form.addEventListener("reset", init);
 init();
 
 function init() {
+
+  // set array elements to null
   boardSquares = [null, null, null, null, null, null, null, null, null];
-  turnOrder = 1; //1 is player X, -1 is player O
-  winner = null; // will hold either 1, -1, or T for tie
+  // set the turnOrder back to 1(X)
+  turnOrder = 1;
+  // set winner to null
+  winner = null; 
+  // reset game message
   gameStatus.innerText = "Let's play! X goes first";
+  // clear out the inner text from the squares and clears the color
   squares.forEach(element => element.innerText = '');
   squares.forEach(element => element.style.backgroundColor = '');
   render();
@@ -43,6 +49,8 @@ function init() {
 
 function render() {
 
+  // set the values in the boardSquares array and bg color of 
+  // square based on the inner text of the square
   for (i = 0; i < boardSquares.length; i++) {
     if (squares[i].innerText === 'X'){
       boardSquares[i] = 1;
@@ -53,11 +61,9 @@ function render() {
     } else {
       boardSquares[i] = null;
     }
-    if (winner === 1) {
-      gameStatus.innerText = 'X has won the game!';
-    } 
-    if (winner === -1) {
-      gameStatus.innerText = 'O has won the game!';
+    // declare end of the game based on winner value
+    if (winner !== null) {
+      gameStatus.innerText = (winner === 1) ? 'X has won the game!' : 'O has won the game!';
     } 
     if (winner === 'T') {
       gameStatus.innerText = 'The games ends in a tie!';
@@ -67,14 +73,18 @@ function render() {
 
 function handleClick(event) {
 
+  // obtain the id from the clicked square on the board and make it an integer
   let i = parseInt(event.target.id);
-
+  // check if square is empty 
   if (boardSquares[i] !== null) {
     return;
   }
+  // check if a winner has been declared
   if (winner !== null || winner === 'T') {
     return;
   }
+  // if square is empty, add an X or O to game board based 
+  // on turn order and change message
   if (boardSquares[i] === null && turnOrder === 1) {
     event.target.innerText = 'X';
     gameStatus.innerText = "It is O's turn"
@@ -84,19 +94,26 @@ function handleClick(event) {
     gameStatus.innerText = "It is X's turn"
   }
 
-  boardSquares[i] = turnOrder; // update board index with the value of turn
+  // Update board index with the value of turn
+  boardSquares[i] = turnOrder; 
+  // Change the turn order to the next player
   turnOrder = turnOrder * -1;
+
   getWinner();
   render();
 }
 
 function getWinner() {
+  // loop through the winningCombo array to count up amount of 1 and -1
   winningCombos.forEach(function (array) {
     let counter = 0;
     array.forEach(function (element) {
       counter += boardSquares[element];
+      // if the amount of 1 or -1 is equal to 3, set winner to the 
+      // value of winning element in boardSquare
       if (Math.abs(counter) === 3) {
         winner = boardSquares[element];
+        // if the board is full and no winner has been declared, change winner to T
       } else if (boardSquares.includes(null) === false && winner === null) {
         winner = 'T';
       }else {
